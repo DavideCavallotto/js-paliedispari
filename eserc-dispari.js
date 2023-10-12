6e6d751e30e432349d7f07433ca8ed3417589846
DavideCavallotto <davidecavallotto92@gmail.com>
2023-10-12 19:01:25 +0200

    add result

--- eserc-dispari.js
+++ eserc-dispari.js
@@ -1,5 +1,7 @@
 // chiedere all'utente di scegliere tra pari e dispari
 const pariDispariUser = prompt('inserisci pari o dispari');
+let pari 
+let dispari 
 console.log(pariDispariUser, 'scelta utente')
 
 // chiedere all'utente di scegliere un numero da 1 a 5
@@ -19,3 +21,31 @@ function getRandomNum(min, max) {
  
 const totNum = numUser + numRandom;
 console.log(totNum)
+
+function isEven(num) {
+    if (num % 2 === 0) {
+        pari = true
+        return pari
+
+    } else {
+        dispari = false
+        return dispari
+
+    }
+}
+
+
+// dichiara chi ha vinto o perso
+if (isEven(totNum)) {
+    console.log('il numero è pari')
+} else {
+    console.log('il numero è dispari')
+}
+
+if (pariDispariUser === pari && numUser === pari) {
+    console.log('Hai Vinto')
+} else if (pariDispariUser === dispari && numUser === dispari) {
+        console.log('Hai Vinto')
+} else {
+        console.log('Hai Perso')
+}
